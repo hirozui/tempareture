30ec27c28042e40a71b493178c975314d1c68d40
hirozui <18171901585@163.com>
2020-09-19 15:55:33 +0800

    score indicator

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,26 +9,66 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-TextView out;
-EditText inp;
+TextView sc;
+Button btn1,btn2,btn3,btn4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        out = (TextView)findViewById(R.id.out);
-        inp = (EditText)findViewById(R.id.inp);
+        sc = (TextView)findViewById(R.id.score);
 
-        Button btn = (Button)findViewById(R.id.btn);
-        btn.setOnClickListener(this);
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        Button btn3 = (Button)findViewById(R.id.btn3);
+        Button btn4 = (Button)findViewById(R.id.btn4);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String score = sc.getText().toString();
+                double num =Double.parseDouble(score);
+                int res;
+                res = (int) (num + 3);
+                sc.setText(String.valueOf(res));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String score = sc.getText().toString();
+                double num =Double.parseDouble(score);
+                int res;
+                res = (int) (num + 2);
+                sc.setText(String.valueOf(res));
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String score = sc.getText().toString();
+                double num =Double.parseDouble(score);
+                int res;
+                res = (int) (num + 1);
+                sc.setText(String.valueOf(res));
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sc.setText("0");
+            }
+        });
     }
 
+
     @Override
-    public void onClick(View v){
-        Log.i("main","onClick msg....");
-        String str = inp.getText().toString();
-        double num = Double.parseDouble(str);
-        double res = (num/5) *9 + 32;
-        out.setText(String.valueOf(res));
+    public void onClick(View view) {
+
     }
 }
